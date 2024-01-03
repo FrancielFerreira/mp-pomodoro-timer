c9bbe3e7c42312c1e980c07e836eb3b120d709d8
nobredev <franciel.ferreira357@gmail.com>
2024-01-03 16:39:15 -0300

    commit initial

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -1,1 +1,88 @@
 // Faça a lógica referente o cronômetro!
+
+const counter = document.getElementById('counter');
+const btnChangePomodoroMode = document.querySelectorAll(
+  '.container-timer .modality',
+);
+
+function changePomodoroMode(event) {
+  event.preventDefault();
+  const btnActive = event.target;
+
+  btnChangePomodoroMode.forEach((btn) => btn.classList.remove('active-button'));
+  btnActive.classList.add('active-button');
+
+  if (btnActive.innerText == 'Intervalo Curto') {
+    counter.innerText = '05:00';
+  } else {
+    counter.innerText = '25:00';
+  }
+}
+
+btnChangePomodoroMode.forEach((btn) =>
+  btn.addEventListener('click', changePomodoroMode),
+);
+
+// countdown
+const btnStartCounter = document.getElementById('start');
+btnStartCounter.addEventListener('click', function startCountdown() {
+  let min = 25;
+  if (counter.innerText === '25:00') min = 25;
+  else min = 5;
+
+  var timerMinutes = 60 * min,
+    display = document.getElementById('counter');
+  handleCountdown(timerMinutes, display);
+});
+
+function handleCountdown(duration, display) {
+  let timer = duration,
+    minutes,
+    seconds;
+  console.log(timer);
+  setInterval(function () {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
+
+    minutes = minutes < 10 ? '0' + minutes : minutes;
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    display.textContent = minutes + ':' + seconds;
+
+    if (--timer < 0) {
+      timer = duration;
+    }
+  }, 1000);
+}
+
+function webSolution() {
+  function startTimer(duration, display) {
+    var timer = duration,
+      minutes,
+      seconds;
+    console.log(timer);
+    setInterval(function () {
+      minutes = parseInt(timer / 60, 10);
+      seconds = parseInt(timer % 60, 10);
+
+      minutes = minutes < 10 ? '0' + minutes : minutes;
+      seconds = seconds < 10 ? '0' + seconds : seconds;
+
+      display.textContent = minutes + ':' + seconds;
+
+      if (--timer < 0) {
+        timer = duration;
+      }
+    }, 1000);
+  }
+  btnStartCounter.addEventListener('click', function () {
+    let min = 25;
+    if (counter.innerText === '25:00') min = 25;
+    else min = 5;
+
+    var timerMinutes = 60 * min,
+      display = document.getElementById('counter');
+    startTimer(timerMinutes, display);
+  });
+}
+// webSolution();
--- assets/js/tasks.js
+++ assets/js/tasks.js
@@ -1,1 +1,69 @@
 // Faça a lógica referente as tarefas aqui!
+const form = document.querySelector('form');
+const btnForm = document.getElementById('add-task');
+const inputTask = document.getElementById('new-task');
+
+form.addEventListener('submit', handleTask);
+btnForm.addEventListener('click', handleTask);
+function handleTask(event) {
+  event.preventDefault();
+  const taskName = inputTask.value;
+
+  createLiItemTask(taskName);
+  saveTask(taskName);
+}
+
+function saveTask(taskName) {
+  let tasks = JSON.parse(localStorage.getItem('tasks'));
+
+  if (tasks === null) {
+    tasks = [];
+  }
+  tasks.push(taskName);
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+
+  inputTask.value = '';
+}
+
+function createLiItemTask(textTask) {
+  const parentElement = document.querySelector('.tasks-list ol');
+
+  const li = document.createElement('li');
+  li.innerText = textTask;
+
+  const span = document.createElement('span');
+  span.innerText = 'X';
+  li.append(span);
+
+  parentElement.append(li);
+}
+
+function insertAllTasksDoc() {
+  let savedTasks = JSON.parse(localStorage.getItem('tasks'));
+  if (savedTasks) {
+    savedTasks.forEach((task) => {
+      createLiItemTask(task);
+    });
+  }
+}
+insertAllTasksDoc();
+
+const closeTask = document.querySelectorAll('#tasks li span');
+
+closeTask.forEach((close) => {
+  close.addEventListener('click', removeTask);
+});
+
+function removeTask() {
+  this.innerText = '';
+
+  const storageTask = JSON.parse(localStorage.getItem('tasks'));
+  const newStorageTask = storageTask.filter((el) => {
+    return el !== this.parentElement.innerText;
+  });
+
+  localStorage.setItem('tasks', JSON.stringify(newStorageTask));
+
+  console.log(this.parentElement.innerText);
+  this.parentElement.remove();
+}
